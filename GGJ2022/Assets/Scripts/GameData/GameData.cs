857c996549e9bf46b13f1b59e3b3ae80d8d2b733
Lowthyst <xcthib@hotmail.com>
2022-01-29 23:49:26 +0100

    Working walls

--- GGJ2022/Assets/Scripts/GameData/GameData.cs
+++ GGJ2022/Assets/Scripts/GameData/GameData.cs
@@ -33,6 +33,7 @@ public class GameData : ScriptableObject
     public float EnemiesSpawnRate;
 
     [Header("Walls")]
+    public List<GameObject> Walls;
     public AnimationCurve WallSpeedCurve;
     public float WallSpeed;
     public Vector2 TimeBetweenWallsSpawns;
