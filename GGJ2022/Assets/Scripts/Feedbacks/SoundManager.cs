5d88ce601ce4aa5000a19b323e4bb16660c9f295
Thibaut <xcthib@hotmail.com>
2022-01-30 16:06:39 +0100

    Add music

--- GGJ2022/Assets/Scripts/Feedbacks/SoundManager.cs
+++ GGJ2022/Assets/Scripts/Feedbacks/SoundManager.cs
@@ -6,13 +6,26 @@ public class SoundManager : Singleton<SoundManager>
 {
     private AudioSource src;
 
+    private int SoundsPerSecond = 30;
+    private int remainingSoundsPerSecond;
+    private float remainingTimeToSecond;
+
     private void Awake()
     {
         src = GetComponent<AudioSource>();
     }
 
+    private void Update()
+    {
+        Timer.LoopedCountDown(ref remainingTimeToSecond, 1, () => remainingSoundsPerSecond = SoundsPerSecond);
+    }
+
     public void PlayClip(AudioClip clip)
     {
-        src.PlayOneShot(clip);
+        if (remainingSoundsPerSecond > 0)
+        {
+            src.PlayOneShot(clip);
+            remainingSoundsPerSecond--;
+        }
     }
 }
