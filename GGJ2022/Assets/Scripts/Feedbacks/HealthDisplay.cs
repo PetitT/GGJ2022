dff22db30ae1fb1c9b7f51573909aedb31aeb9fb
Lowthyst <xcthib@hotmail.com>
2022-01-30 05:15:39 +0100

    Add health display

--- GGJ2022/Assets/Scripts/Feedbacks/HealthDisplay.cs
+++ GGJ2022/Assets/Scripts/Feedbacks/HealthDisplay.cs
@@ -2,10 +2,12 @@ using System.Collections;
 using System.Collections.Generic;
 using TMPro;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class HealthDisplay : MonoBehaviour
 {
     [SerializeField] private TMP_Text healthText;
+    [SerializeField] private List<Image> healthImages;
 
     private void Start()
     {
@@ -20,6 +22,11 @@ public class HealthDisplay : MonoBehaviour
 
     private void HealthManager_onHealthChanged(int newHealth)
     {
+        if (newHealth == GameManager.Instance.Data.CharacterMaxHealth) return;
         healthText.text = newHealth.ToString();
+        if (healthImages.Count >= newHealth - 2)
+        {
+            healthImages[newHealth].gameObject.SetActive(false);
+        }
     }
 }
